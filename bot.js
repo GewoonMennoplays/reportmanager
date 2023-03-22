e13cc9ba8f1106a396ed32f4ccd3a0c8de19354b
GewoonMennoplays <menno.lust@gmail.com>
2023-03-22 15:17:46 +0100

    Added console log function

--- bot.js
+++ bot.js
@@ -28,6 +28,7 @@ for (const file of commandFiles) {
 
   if('data' in command && 'execute' in command) {
     client.commands.set(command.data.name, command);
+    console.log("Loaded command: " + command.data.name)
   } else {
     console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
   }
