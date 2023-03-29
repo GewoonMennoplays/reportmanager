076afdbf6516b962006259a2babf9f09f46cc5b7
GewoonMennoplays <menno.lust@gmail.com>
2023-03-29 21:38:37 +0200

    Update

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,18 +1,34 @@
 const Discord = require('discord.js');
 const moment = require('moment');
+const mysql = require('mysql');
 
-module.exports = (client, guild) => {
-  const guildCreateChannel = client.channels.cache.get('1090718415200731177');
-  if (!guildCreateChannel) return console.error('Unable to find channel.');
+const client = new Discord.Client();
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'mydatabase'
+});
+
+client.on('guildCreate', guild => {
 
   const embed = new Discord.MessageEmbed()
-    .setColor('GREEN')
-    .setTitle(`New Guild: ${guild.name}`)
-    .setDescription(`Guild ID: ${guild.id}`)
-    .addField('Owner', `${guild.owner.user.tag} (${guild.ownerID})`, true)
-    .addField('Members', `${guild.memberCount}`, true)
-    .addField('Boosts', `${guild.premiumSubscriptionCount}`, true)
-    .addField('Created At', `${moment(guild.createdAt).format('MMMM Do YYYY, h:mm:ss a')}`, true);
+    .setTitle(`Joined guild: ${guild.name}`)
+    .addFields(
+      { name: 'Guild ID', value: guild.id, inline: true },
+      { name: 'Guild Owner', value: `${guild.owner.user.username}#${guild.owner.user.discriminator} (ID: ${guild.owner.id})`, inline: true },
+      { name: 'Member Count', value: guild.memberCount, inline: true },
+      { name: 'Boost Count', value: guild.premiumSubscriptionCount, inline: true },
+      { name: 'Join Date', value: moment(guild.joinedAt).format('MMM Do YYYY, h:mm:ss a'), inline: true }
+      )
 
-  guildCreateChannel.send(embed);
-};
+  client.channels.cache.get('1090718415200731177').send(embed);
+
+  const query = `INSERT INTO guilds (guild_name, guild_id, owner_name, owner_id, is_blacklisted) VALUES (?, ?, ?, ?, ?)`;
+  const values = [guild.name, guild.id, guild.owner.user.username, guild.owner.id, false];
+  
+  connection.query(query, values, (error, results, fields) => {
+    if (error) throw error;
+    console.log(`Added guild ${guild.name} to database.`);
+  });
+});
